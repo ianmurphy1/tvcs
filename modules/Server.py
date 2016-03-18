b5110c484cc3a6db3f2debd262ec814292c8ddfe
Ian Murphy <iano200@gmail.com>
2016-03-18 09:31:57 +0000

    removed server messages

--- modules/Server.py
+++ modules/Server.py
@@ -13,6 +13,7 @@ Obj = DynamicObjectV2.Class
 # put your imports here
 import socket
 import jsonpickle
+import struct
 
 def init(self):
     # put your self.registerOutput here
@@ -21,13 +22,14 @@ def init(self):
 
 
 def run (self):
+
     # put your init and global variables here
     s = socket.socket()
     host = ''
     port = 12345
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     s.bind((host, port))
-
     s.listen(5)
     # main loop
     while 1:
@@ -35,16 +37,16 @@ def run (self):
         # you can use: output, getInputs, message
         c, addr = s.accept()
         self.message('Got connection from {}'.format(addr))
+        print 'look for connection loop'
 
         while 1:
-            data, _ = c.recvfrom(4096)
+            print 'server while loop'
+            data = recv_one_message(c)
             if not data:
                 self.message('connection broke')
                 break
-            print 'size of receiving: {}'.format(len(data))
             r_obj = jsonpickle.decode(data)
-            print r_obj
-            addToMemory(self, "facePos1", r_obj)
+            addToMemory(self, r_obj['tag'], r_obj['data'])
 
         c.close()
 
@@ -56,3 +58,22 @@ def addToMemory(self, key, obj):
 
 def checkMemory(self, key):
     return self.getInputs()[key]
+
+def send_one_message(sock, data):
+    length = len(data)
+    sock.sendall(struct.pack('!I', length))
+    sock.sendall(data)
+
+def recv_one_message(sock):
+    lengthbuf = recvall(sock, 4)
+    length, = struct.unpack('!I', lengthbuf)
+    return recvall(sock, length)
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
